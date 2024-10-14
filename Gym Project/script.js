c6c12c5bea029429765ea675d2a1057df9ca6789
BotanDaniel <125061968+BotanDaniel@users.noreply.github.com>
2024-10-14 15:07:49 +0300

    Add files via upload

--- Gym Project/script.js
+++ Gym Project/script.js
@@ -1,6 +1,19 @@
 "use strict";
+const hiddenElements = document.querySelectorAll(".hidden");
 
 function toggleMenu() {
   const menu = document.querySelector(".navbar--links__container");
   menu.classList.toggle("active");
 }
+
+//
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
+hiddenElements.forEach((el) => observer.observe(el));
